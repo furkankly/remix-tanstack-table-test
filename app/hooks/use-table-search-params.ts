a148ebdb81009220358304f1b2930da041c565fa
Oğuzhan Durgun <oguzhandurgun95@gmail.com>
2023-12-14 13:20:01 +0300

    Add logs
    Signed-off-by: Oğuzhan Durgun <oguzhandurgun95@gmail.com>

--- app/hooks/use-table-search-params.ts
+++ app/hooks/use-table-search-params.ts
@@ -63,22 +63,49 @@ const getPaginationFromSearchParams = (
   defaultState: PaginationState
 ): PaginationState => {
   const pageIndexParam: string | null = searchParams.get(pageIndexParamName);
+  console.log(
+    `getPaginationFromSearchParams(): pageIndexParam is ${pageIndexParam}`
+  );
   const pageSizeParam: string | null = searchParams.get(pageSizeParamName);
+  console.log(
+    `getPaginationFromSearchParams(): pageSizeParam is ${pageSizeParam}`
+  );
 
   let pageIndex: number;
   if (!pageIndexParam) {
+    console.log(
+      `getPaginationFromSearchParams(): pageIndexParam is not set, setting it to the default value ${defaultState.pageIndex}`
+    );
     pageIndex = defaultState.pageIndex;
   } else {
+    console.log(
+      `getPaginationFromSearchParams(): pageIndexParam is set, setting it to the value ${parseInt(
+        pageIndexParam,
+        10
+      )}`
+    );
     pageIndex = parseInt(pageIndexParam, 10);
   }
 
   let pageSize: number;
   if (!pageSizeParam) {
+    console.log(
+      `getPaginationFromSearchParams(): pageSizeParam is not set, setting it to the default value ${defaultState.pageSize}`
+    );
     pageSize = defaultState.pageSize;
   } else {
+    console.log(
+      `getPaginationFromSearchParams(): pageSizeParam is set, setting it to the value ${parseInt(
+        pageSizeParam,
+        10
+      )}`
+    );
     pageSize = parseInt(pageSizeParam, 10);
   }
 
+  console.log(
+    `getSortFromSearchParams(): returning {pageIndex: ${pageIndex}, pageSize: ${pageSize}}`
+  );
   return {
     pageIndex,
     pageSize,
@@ -92,23 +119,44 @@ const getSortFromSearchParams = (
   const sortDescendingParam: string | null = searchParams.get(
     sortDescendingParamName
   );
+  console.log(
+    `getSortFromSearchParams(): sortDescendingParam is ${sortDescendingParam}`
+  );
 
   const sortIdParam: string | null = searchParams.get(sortIdParamName);
+  console.log(`getSortFromSearchParams(): sortIdParam is ${sortIdParam}`);
 
   let desc: boolean;
   if (!sortDescendingParam) {
+    console.log(
+      `getSortFromSearchParams(): sortDescendingParam is not set, setting it to the default value ${defaultState.desc}`
+    );
     desc = defaultState.desc;
   } else {
+    console.log(
+      `getSortFromSearchParams(): sortDescendingParam is set, setting it to the value ${
+        sortDescendingParam === "true" || sortDescendingParam === ""
+      }`
+    );
     desc = sortDescendingParam === "true" || sortDescendingParam === "";
   }
 
   let id: string;
   if (!sortIdParam) {
+    console.log(
+      `getSortFromSearchParams(): sortIdParam is not set, setting it to the default value ${defaultState.id}`
+    );
     id = defaultState.id;
   } else {
+    console.log(
+      `getSortFromSearchParams(): sortDescendingParam is set, setting it to the value ${sortIdParam}`
+    );
     id = sortIdParam;
   }
 
+  console.log(
+    `getSortFromSearchParams(): returning {id: ${id}, desc: ${desc}}`
+  );
   return {
     id,
     desc,
